e14d322a1a311b47bac06b297d34ffdd29bfbdc5
Ankit Tiwari <ankitt255@gmail.com>
2019-01-14 15:38:16 +0530

    🧼clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import HeroImage from '../images/svg/hero-img.svg'
+import Link from 'gatsby-link'
 
 const IndexPage = () => {
   return (
@@ -14,7 +15,7 @@ const IndexPage = () => {
             <div className="hero-text__heading">
               <h1>Hi! I am Asha.</h1>
               <h2>I make beautiful things with a pencil.</h2>
-              <a href="#" className="btn btn--pink btn--animated">See my Work</a>
+              <Link to="/sketches" className="btn btn--pink btn--animated">See my Work</Link>
             </div>
           </div>
         </div>
