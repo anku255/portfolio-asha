fa956995f62bbd850239484ff290c26593ea2f44
Ankit Tiwari <ankitt255@gmail.com>
2019-09-15 16:24:29 +0530

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import HeroImage from '../images/svg/artist-img.svg'
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <section className="about-page section__hero">
+        <div className="columns">
+          <div className="column hero-img">
+            <HeroImage className="hero-img-svg" />
+          </div>
+          <div className="column hero-text">
+            <p>
+              Hi! I am Asha Sharma. I am a sketch artist by passion and software
+              enginner by profession. You can find me on{' '}
+              <a href="https://www.instagram.com/beinganartist/">Instagram</a>,{' '}
+              <a href="https://www.facebook.com/beinganartist/">Facebook</a>,
+              and{' '}
+              <a href="https://www.youtube.com/channel/UCeDg588ylKWdGuIF-n94-7Q">
+                Youtube
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default IndexPage
