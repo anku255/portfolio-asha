d8ecbfd91de7a0fdf191a860be5c14e887909093
Ankit Tiwari <ankitt255@gmail.com>
2019-09-15 18:05:42 +0530

    sort pages by createdAt

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,14 +36,9 @@ exports.createPages = ({ graphql, actions }) => {
               sort: { fields: [photo___file___fileName], order: ASC }
             ) {
               edges {
-                previous {
-                  slug
-                }
-                next {
-                  slug
-                }
                 node {
                   slug
+                  createdAt
                 }
               }
               totalCount
@@ -60,29 +55,49 @@ exports.createPages = ({ graphql, actions }) => {
 
         const { totalCount } = allContentfulAsset
 
+        const pages = []
+
         for (let i = 0; i < totalCount; i++) {
           const localFilePath =
             allContentfulAsset.edges[i].node.localFile.absolutePath
           const slug = allContentfulImage.edges[i].node.slug
-          const prev = allContentfulImage.edges[i].previous
-          const next = allContentfulImage.edges[i].next
+          const createdAt = allContentfulImage.edges[i].node.createdAt
 
           // get the dominant color
 
           const rgbColor = await ColorThief.getColor(localFilePath)
           const hexColor = rgbToHex(rgbColor)
 
-          createPage({
+          const page = {
             path: `/image/${slug}/`,
             component: Image,
             context: {
               backgroundColor: hexColor,
               slug: slug,
-              prev: prev && prev.slug,
-              next: next && next.slug,
             },
+          }
+
+          pages.push({
+            page,
+            createdAt,
           })
         }
+
+        // sort the pages by createdAt (desc)
+        pages.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+        pages.forEach(({ page }, index) => {
+          createPage({
+            ...page,
+            context: {
+              ...page.context,
+              prev: index === 0 ? null : pages[index - 1].page.context.slug,
+              next:
+                index === pages.length - 1
+                  ? null
+                  : pages[index + 1].page.context.slug,
+            },
+          })
+        })
       })
     )
   })
